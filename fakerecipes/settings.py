aeba01e96762eddad5611a703e01c8755d87670c
serhat <vuralser@itu.edu.tr>
2020-03-27 13:08:34 +0300

    db setup fix

--- fakerecipes/settings.py
+++ fakerecipes/settings.py
@@ -77,7 +77,10 @@ WSGI_APPLICATION = 'fakerecipes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES = {
+    'default':  dj_database_url.config(conn_max_age=600, ssl_require=True)
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
